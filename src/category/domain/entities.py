f710486ddde06bc33c8d9e9a47871aeed0a41563
mades eladrian <mades.eladrian@gmail.com>
2022-10-04 13:26:47 +0000

    first commit

---
+++ src/category/domain/entities.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+@dataclass
+class Category:
+  name: str
+  description: Optional[str] = None
+  is_active: Optional[bool] = True
+  created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
