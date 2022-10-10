06126f85e9e39a5e332588d6856898360186a600
mades eladrian <mades.eladrian@gmail.com>
2022-10-10 14:40:34 -0300

    ensure Entity is an abstract class

--- src/__seedwork/tests/unit/domain/test_unit_entities.py
+++ src/__seedwork/tests/unit/domain/test_unit_entities.py
@@ -1,9 +1,12 @@
+from abc import ABC
 from dataclasses import is_dataclass
 import unittest
 from src.__seedwork.domain.entities import Entity
-
 
 class TestEntityUnit(unittest.TestCase):
   
   def test_if_is_a_dataclass(self):
     self.assertTrue(is_dataclass(Entity))
+    
+  def test_if_is_an_abstract_class(self):
+    self.assertIsInstance(Entity(), ABC)
