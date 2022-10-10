c125611a1ec754e5d5dc8ebc6b86f60b1aff6253
mades eladrian <mades.eladrian@gmail.com>
2022-10-10 14:46:04 -0300

    ensure Entity set unique entity id and props

--- src/__seedwork/tests/unit/domain/test_unit_entities.py
+++ src/__seedwork/tests/unit/domain/test_unit_entities.py
@@ -1,7 +1,14 @@
 from abc import ABC
-from dataclasses import is_dataclass
+from dataclasses import dataclass, is_dataclass
 import unittest
+
 from src.__seedwork.domain.entities import Entity
+from src.__seedwork.domain.value_objects import UniqueEntityId
+
+@dataclass(frozen=True, kw_only=True)
+class StubEntity(Entity):
+  prop1: str
+  prop2: str
 
 class TestEntityUnit(unittest.TestCase):
   
@@ -10,3 +17,10 @@ class TestEntityUnit(unittest.TestCase):
     
   def test_if_is_an_abstract_class(self):
     self.assertIsInstance(Entity(), ABC)
+    
+  def test_set_unique_entity_id_and_props(self):
+    entity = StubEntity(prop1='value1', prop2='value2')
+    self.assertEqual(entity.prop1, 'value1')
+    self.assertEqual(entity.prop2, 'value2')
+    self.assertIsInstance(entity.unique_entity_id, UniqueEntityId)
+    self.assertEqual(entity.unique_entity_id.id, entity.id)
