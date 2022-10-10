111aa47f0c4b54f761fce1794058b66306c7a848
mades eladrian <mades.eladrian@gmail.com>
2022-10-10 10:48:17 -0300

    add UniqueEntityId

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -1,10 +1,16 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Optional
+import uuid
 
-@dataclass
+@dataclass(kw_only=True)
 class Category:
+  id: uuid.UUID = field(
+    default_factory=lambda: uuid.uuid4()
+  )
   name: str
   description: Optional[str] = None
   is_active: Optional[bool] = True
-  created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
+  created_at: Optional[datetime] = field(
+    default_factory=lambda: datetime.now()
+  )
